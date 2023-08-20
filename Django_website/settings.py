792b83f0a1d1bb6d1bd1736c8b1db72ea3b249b5
KC_software_en <kisha.cairncross@gmail.com>
2023-08-20 23:22:22 +0100

    Removed SECRET_KEY and put it in secret.txt

--- Django_website/settings.py
+++ Django_website/settings.py
@@ -21,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+##############################################################
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf2f%sn9ktlg#76uasqot*sb-x4inu4^s+5hr4zd*=!8jjh^hb'
+# SECRET_KEY = removed and placed in a text file in dropbox
+################################################################
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # comment out true when in development
